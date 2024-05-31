3afb5c05c32de6c82773931ede0147c99063bfc8
14H <do.duc.hieu@linecorp.com>
2024-05-31 19:51:20 +0700

    Remove default value model API root

--- core/src/commonMain/kotlin/dev/kryptonreborn/blockfrost/health/model/ApiRoot.kt
+++ core/src/commonMain/kotlin/dev/kryptonreborn/blockfrost/health/model/ApiRoot.kt
@@ -4,6 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ApiRoot(
-    val url: String = "",
-    val version: String = "",
+    val url: String,
+    val version: String,
 )
