9fc5a3362295358df06553b82d49d68adcfe1e5d
14H <doduchieu.kstn@gmail.com>
2024-06-14 21:36:58 +0700

    Update asset for integration test

--- core/src/commonTest/kotlin/dev/kryptonreborn/blockfrost/integrationtest/CardanoAddressApiIntegrationTest.kt
+++ core/src/commonTest/kotlin/dev/kryptonreborn/blockfrost/integrationtest/CardanoAddressApiIntegrationTest.kt
@@ -11,7 +11,7 @@ import kotlin.test.assertTrue
 class CardanoAddressApiIntegrationTest : BaseIntegrationTest() {
     private val address =
         "addr1qxqs59lphg8g6qndelq8xwqn60ag3aeyfcp33c2kdp46a09re5df3pzwwmyq946axfcejy5n4x0y99wqpgtp2gd0k09qsgy6pz"
-    private val asset = ""
+    private val asset = "00000002df633853f6a47465c9496721d2d5b1291b8398016c0e87ae6e7574636f696e"
 
     @Test
     fun testGetAddress() = runIntegrationTest {
